15f6c3b87fcf23659641926df1d632235b0adf06
ScramblesDev <beesley02@gmail.com>
2023-12-09 19:35:41 -0500

    added more functionality + comments

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import password_generator as pg
 
 def main():
-    print("Welcome to the Password Generator!")
-    length = int(input("How long would you like the password to be? "))
-    use_advanced = input("Would you like to include advanced character sets (lowercase/uppercase/special symbols)? (yes/no) ").lower()
-    strength = input("How strong would you like the password to be (high/medium/low randomness)? ").lower()
-    pronounceable = input("Do you want pronounceable passwords? (yes/no) ").lower()
+    print("Welcome to the Advanced Password Generator!")
 
-    # generates the password
-    password = pg.generate_password(length, use_advanced == 'yes', strength, pronounceable == 'yes')
+    # we're gonna ask the user for the length
+    length = int(input("How long would you like the password to be? "))
+    # character set
+    use_advanced = ask_yes_no("Would you like to include an advanced character set (lowercase/uppercase/special symbols)?")
+    use_custom_chars = ask_yes_no("Would you like to specify a custom character set?")
+    custom_chars = input("Enter the custom characters: ") if use_custom_chars else None
+    # password strength
+    strength = ask_strength()
+    # pronounceability
+    pronounceable = ask_yes_no("Do you want a pronounceable password?")
+    # generation
+    password = pg.generate_password(length, use_advanced, strength, pronounceable, custom_chars)
     print("Your generated password is:", password)
+    print("Estimated password entropy:", pg.calculate_entropy(password))
+
+def ask_yes_no(question):
+    return input(f"{question} (yes/no) ").lower() == 'yes'
+
+def ask_strength():
+    options = ['low', 'medium', 'high']
+    while True:
+        strength = input("Choose the password strength (low/medium/high): ").lower()
+        if strength in options:
+            return strength
+        else:
+            print("Invalid option. Please choose 'low', 'medium', or 'high'.")
 
 if __name__ == "__main__":
     main()
